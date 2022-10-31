816b8b32539ad8ad016cdff520a48397e56cde97
Léo Coletta <contact@leo-coletta.fr>
2022-10-31 17:36:11 +0100

    fix(nuxt): Fix nuxt types

--- packages/nuxt-kit/src/runtime/types/write.d.ts
+++ packages/nuxt-kit/src/runtime/types/write.d.ts
@@ -4,3 +4,10 @@ declare module '#lyonkit' {
   export const lyonkitConfig: { apiKey?: string, readOnly: boolean };
   export function useLyonkit(): ReturnType<typeof createLyonkitClient>;
 }
+
+declare module '#imports' {
+  import { createLyonkitWriteApiClient as createLyonkitClient } from '@lyonkit/ts-client'
+
+  export function useLyonkit(): ReturnType<typeof createLyonkitClient>
+  export const lyonkitConfig: { apiKey?: string, readOnly: boolean };
+}
